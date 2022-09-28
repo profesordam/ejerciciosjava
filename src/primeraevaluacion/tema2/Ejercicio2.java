f694fe28a4e1e9cb5b75c01e617429b577e062ac
Julio Pedro López Bayón <jplb.fp@gmail.com>
2022-09-28 17:41:58 +0200

    Clase 28 de septiembre de 2022

---
+++ src/primeraevaluacion/tema2/Ejercicio2.java
@@ -0,0 +1,11 @@
+package primeraevaluacion.tema2;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		double div = 1234d / 532;
+		System.out.printf("1234 / 532 = %-15.2f", div);
+		
+	}
+
+}
