58aa0fafec4a3891b4e26358cf135fc9eedc787a
Julio Pedro López Bayón <jplb.fp@gmail.com>
2022-10-10 21:39:11 +0200

    Clase del 10 de octubre de 2022

---
+++ src/primeraevaluacion/ejemplos/Saludo.java
@@ -0,0 +1,9 @@
+package primeraevaluacion.ejemplos;
+
+public class Saludo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola " + args[0]);
+	}
+	
+}
--- src/primeraevaluacion/tema2/Ejercicio26.java
+++ src/primeraevaluacion/tema2/Ejercicio26.java
@@ -6,20 +6,33 @@ import java.io.InputStreamReader;
 
 public class Ejercicio26 {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		double x1 = leerCoordenada("x1: ", in);
+		double y1 = leerCoordenada("y1: ", in);
+		double x2 = leerCoordenada("x2: ", in);
+		double y2 = leerCoordenada("y2: ", in);
+		double d = distancia(x1, y1, x2, y2);
+		System.out.println("distancia: " + d);
+	}
+			
+	static double leerCoordenada(String msg, BufferedReader in)  {
+		boolean correcto;
+		double valor = 0;
+		System.out.print(msg);
+		do {
+			correcto = true; 
+			try {
+				valor = Double.parseDouble(in.readLine());
+			} catch (NumberFormatException | IOException e) {
+				System.out.println("número incorrecto, inténtalo de nuevo");
+				correcto = false;
+			}
+		} while (!correcto);
+		return valor;
 	}
 	
-	static double distancia() throws NumberFormatException, IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("x1: ");
-		double x1 = Double.parseDouble(in.readLine());
-		System.out.print("y1: ");
-		double y1 = Double.parseDouble(in.readLine());
-		System.out.print("x2: ");
-		double x2 = Double.parseDouble(in.readLine());
-		System.out.print("y2: ");
-		double y2 = Double.parseDouble(in.readLine());
+	static double distancia(double x1, double y1, double x2, double y2) {
 		return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 	}
 
