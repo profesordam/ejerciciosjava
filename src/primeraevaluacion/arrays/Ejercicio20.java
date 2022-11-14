f0e43a8f61dd6f7929f5a497be71d5d81670f66c
Julio Pedro López Bayón <jplb.fp@gmail.com>
2022-11-14 18:38:25 +0100

    Clase del 11 de noviembre de 2022

--- src/primeraevaluacion/arrays/Ejercicio20.java
+++ src/primeraevaluacion/arrays/Ejercicio20.java
@@ -14,8 +14,6 @@ public class Ejercicio20 {
 		while (!fin) {
 			System.out.print("> ");
 			fin = ejecutarComando(in.nextLine());
-			for (int i=0; i<matriz.length; i++)
-				System.out.println(Arrays.toString(matriz[i]));
 		}
 		in.close();
 	}
@@ -36,30 +34,64 @@ public class Ejercicio20 {
 			}
 		} catch (NoSuchElementException e) {
 			System.out.println("comando incorrecto");
+		} finally {
+			scanner.close();
 		}
-		scanner.close();
 		return false;
 	}
+	
+	/*
+	 * matriz 2x3: 1 2 3 4 5 6
+	 */
 	
 	static void matriz(Scanner scanner) {
 		try {
 			scanner.skip("\\p{javaWhitespace}+");
 			int filas = Integer.parseInt(scanner.skip("\\d+").match().group());
-			scanner.skip("x");
+			scanner.skip("\\p{javaWhitespace}*x\\p{javaWhitespace}*");
 			int columnas = Integer.parseInt(scanner.skip("\\d+").match().group());
-			scanner.skip(":");
+			scanner.skip("\\p{javaWhitespace}*:\\p{javaWhitespace}*");
 			matriz = new int[filas][columnas];
 			for (int i=0; i<filas; i++)
 				for (int j=0; j<columnas; j++)
 					matriz[i][j] = scanner.nextInt();
+			if (scanner.hasNext()) {
+				System.out.println("comando incorrecto: demasiados números");
+				matriz = null;
+			}
+			
 		} catch (NoSuchElementException e) {
 			System.out.println("comando incorrecto");
 		}
 		
 	}
 	
+	
+	/*
+	 * intercambia 0 0 1 1
+	 */
 	static void intercambia(Scanner scanner) {
-		
+		try {
+			int f0 = scanner.nextInt();
+			int c0 = scanner.nextInt();
+			int f1 = scanner.nextInt();
+			int c1 = scanner.nextInt();
+			if (scanner.hasNext()) 
+				System.out.println("comando incorrecto: demasiados números");
+			else {
+				int aux = matriz[f0][c0];
+				matriz[f0][c0] = matriz[f1][c1];
+				matriz[f1][c1] = aux;
+				mostrarMatriz();
+			}
+		} catch (NoSuchElementException e) {
+			System.out.println("comando incorrecto");
+		}
+	}
+	
+	static void mostrarMatriz() {
+		for (int i=0; i<matriz.length; i++)
+			System.out.println(Arrays.toString(matriz[i]));
 	}
 
 }
