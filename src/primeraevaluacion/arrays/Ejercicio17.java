012310a8fc11fcaa0ad2cde9170a3b9d1b09e572
Julio Pedro López Bayón <jplb.fp@gmail.com>
2022-11-18 20:45:24 +0100

    Clase del 18 de noviembre de 2022

--- src/primeraevaluacion/arrays/Ejercicio17.java
+++ src/primeraevaluacion/arrays/Ejercicio17.java
@@ -11,9 +11,9 @@ import java.util.Arrays;
 public class Ejercicio17 {
 
 	public static void main(String[] args) {
-		int longitud = (int) (Math.random() * 90 + 10);
+		int longitud = (int) (Math.random() * 13 + 3);
 		int [] v1 = new int[longitud];
-		longitud = (int) (Math.random() * 90 + 10);
+		longitud = (int) (Math.random() * 13 + 3);
 		int [] v2 = new int[longitud];
 		rellenar(v1);
 		rellenar(v2);
@@ -23,14 +23,21 @@ public class Ejercicio17 {
 		int i1 = 0;
 		int i2 = 0;
 		for (int i=0; i<v3.length; i++) {
-			
+			if (i1 == v1.length || (i2 < v2.length && v2[i2] <= v1[i1]))
+				v3[i] = v2[i2++];
+			else if (i2 == v2.length || (i1 < v1.length && v1[i1] < v2[i2]))
+				v3[i] = v1[i1++];
 		}
+		System.out.println(Arrays.toString(v1));
+		System.out.println(Arrays.toString(v2));
+		System.out.println(Arrays.toString(v3));
 	}
 	
 	static void rellenar(int [] v) {
 		for (int i=0; i<v.length; i++) {
-			v[i] = (int) (Math.random() * Integer.MAX_VALUE); 
+			v[i] = (int) (Math.random() * 100); 
 		}
 	}
 
+	
 }
