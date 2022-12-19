3122b7a1c1be0fce84576b6b301bab03ae04f018
Julio Pedro López Bayón <jplb.fp@gmail.com>
2022-12-19 18:55:27 +0100

    Clase del 19 de diciembre de 2022

---
+++ src/primeraevaluacion/regex/Ejercicio1.java
@@ -0,0 +1,23 @@
+package primeraevaluacion.regex;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		Pattern regex = Pattern.compile("\\b[A-ZÁÉÍÓÚ][a-záéíóú]+(?: \\b[A-ZÁÉÍÓÚ][a-záéíóú]+)+\\b");
+//		Pattern regex = Pattern.compile("\\b([A-ZÁÉÍÓÚÜ][a-záéíóúü]+)(?: \\1)+\\b");
+		System.out.println("Nombres:");
+		Matcher m = regex.matcher(in.readLine());
+		while (m.find())
+			System.out.println(m.group());
+	}
+
+}
+
+// María Fernández Pérez, Alberto González, Mariano Martínez, Laura Sánchez López
